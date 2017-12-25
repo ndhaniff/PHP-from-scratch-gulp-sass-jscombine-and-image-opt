edbaee74bd57a67f929d7d8d8237ede2f21937b6
endy <ndhaniff@gmail.com>
2017-12-26 02:56:41 +0800

    remove pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,6 @@ const gulp = require('gulp')
 const browsersync = require('browser-sync').create()
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer');
-const jade = require('gulp-jade')
-
-//compile html
-gulp.task('jade',function(){
-  return gulp.src('src/templates/*.jade')
-  .pipe(jade({pretty: true}))
-  .pipe(gulp.dest('src'))
-  .pipe(browsersync.stream())
-})
 
 
 //compile sass
@@ -26,11 +17,10 @@ gulp.task('sass',function(){
 })
 
 //watch serve
-gulp.task('serve', ['jade','sass'] ,function(){
+gulp.task('serve', ['sass'] ,function(){
   browsersync.init({
     server: './src'
   })
-  gulp.watch(['src/templates/*.jade'], ['jade'])
   gulp.watch(['src/scss/*.scss'], ['sass'])
   gulp.watch(['src/*.html']).on('change',browsersync.reload)
 })
